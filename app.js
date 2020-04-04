25574d18d8016a1bdef0a1627b76093eb741d1da
Dylan Barratt <dylanbarratt@catlover.com>
2020-04-04 15:21:32 +0100

    Fixed header sizing issues

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const body = document.body;
+const toggle = document.getElementById("toggle");
 
 function changeTheme() {
     if (body.classList.contains("light")) {
         body.className = "dark";
+        toggle.innerText = "🌞";
     } else {
         body.className = "light";
+        toggle.innerText = "🌚";
     }
 }
