1e443ac4f43f6ea8c8a77a866e7048a5c6e76ca5
Dylan Barratt <dylanbarratt@catlover.com>
2020-04-05 12:06:30 +0100

    added gif images

--- app.js
+++ app.js
@@ -20,3 +20,19 @@ function changeTheme() {
         localStorage.setItem("theme", "light");
     }
 }
+
+function conwayGIF(img, show) {
+    if (show) {
+        img.src = "images/conway.gif";
+    } else {
+        img.src = "images/conway.jpg";
+    }
+}
+
+function snakeGIF(img, show) {
+    if (show) {
+        img.src = "images/snake.gif";
+    } else {
+        img.src = "images/snake.jpg";
+    }
+}
