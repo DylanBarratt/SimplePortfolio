ba99452ff26ebd9079e70098cf48c1f2509a06ef
Dylan Barratt <dylanbarratt@catlover.com>
2020-04-05 11:28:37 +0100

    added theme storage

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const body = document.body;
 const toggle = document.getElementById("toggle");
 
+if (localStorage.getItem("theme") == "dark") {
+    body.className = "dark";
+    toggle.innerText = "🌞";
+} else {
+    body.className = "light";
+    toggle.innerText = "🌚";
+}
+
 function changeTheme() {
     if (body.classList.contains("light")) {
         body.className = "dark";
         toggle.innerText = "🌞";
+        localStorage.setItem("theme", "dark");
     } else {
         body.className = "light";
         toggle.innerText = "🌚";
+        localStorage.setItem("theme", "light");
     }
 }
